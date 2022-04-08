941288ca881d0a84f60b5fcf2bbf4f7c89adb798
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-09 00:02:43 +0200

    Add Exercise 02

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,11 +1,15 @@
 function myCounter(n){
-    if(n<1){
+    if(n < 1){
         return [];
     }else{
-        const arr = myCounter(n-1);
-        arr.push(n);
+        var arr = myCounter(n-1);
+        arr.unshift(n);
+        return arr;
     }
 
 }
 
 console.log(myCounter(5));
+console.log(myCounter(10));
+console.log(myCounter(-1));
+module.exports = myCounter;
