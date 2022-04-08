a2e3207a21d26c4f7f828e79fed9fe070b9d65a0
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-08 20:32:30 +0200

    Add Exercise 04

---
+++ ex04/src/index.js
@@ -0,0 +1,27 @@
+function sumFibonacci(num){
+    if (num < 0){
+        return 0;
+    }else if (num == 1){
+        return 1;
+    } else{
+    let previous = 0;
+    let current = 1;
+    let result = 0;
+    while (current <= num) {
+      if (current % 2 !== 0) {
+        result += current;
+      }
+      current += previous;
+      previous = current - previous;
+    }
+  
+    return result;
+}
+}
+
+console.log(sumFibonacci(1));
+console.log(sumFibonacci(10));
+console.log(sumFibonacci(20));
+console.log(sumFibonacci(4));
+console.log(sumFibonacci(-5));
+module.exports = sumFibonacci;
