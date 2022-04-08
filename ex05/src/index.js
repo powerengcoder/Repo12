c76a3000ea18ea754343b7347fbdb57db6fd88e4
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-08 20:42:25 +0200

    Add Exercise 05

---
+++ ex05/src/index.js
@@ -0,0 +1,25 @@
+var myPetsArray = [
+
+{
+animalType : "Dog",
+name: "Pujdo"
+},
+{
+    animalType : "Cat",
+    name: "Maca"
+},
+{
+    animalType : "Bird",
+    name: "Tweety"
+}
+];
+
+function myPetsFunction(pets){
+return pets.name;
+}
+
+console.log(myPetsFunction(myPetsArray[0]));
+console.log(myPetsFunction(myPetsArray[1]));
+console.log(myPetsFunction(myPetsArray[2]));
+module.exports = myPetsFunction;
+module.exports = myPetsArray;
