33866bd454a6ab68306965012a36e7c85f8d47c9
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-09 00:16:52 +0200

    Add Exercise 04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,9 +1,9 @@
-function sumFibonacci(num){
-    if (num < 0){
-        return 0;
-    }else if (num === 1){
-        return 1;
-    } else{
+function sumFibonacci(num) {
+  if (num < 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
     let previous = 0;
     let current = 1;
     let result = 0;
@@ -14,9 +14,9 @@ function sumFibonacci(num){
       current += previous;
       previous = current - previous;
     }
-  
+
     return result;
-}
+  }
 }
 
 console.log(sumFibonacci(1));
